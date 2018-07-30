81774ab0d21b42ffae8ad4423d535722ba31ac0f
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-30 21:17:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,8 +19,19 @@ function pressIt(){
   })
 }
 
+function submitIt() {
+  $('form').on('submit', function() {
+    if ($("input:first").val() === "correct") {
+      alert('your form is going to be submitted now');
+      return;
+    }
+  })
+}
+
+
 $(document).ready(function(){
-
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt(); 
 });
